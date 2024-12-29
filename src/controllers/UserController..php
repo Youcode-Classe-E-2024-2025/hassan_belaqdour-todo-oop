ed5dde79a1df727e59b6b7db4d7d70229c630b30
ismail baoud <ibaoud46@gmail.com>
2024-12-29 17:26:25 +0100

    fixe tous les error dans la page de sign up et ajouter les utilisateur a la ase de donne

--- src/controllers/UserController..php
+++ src/controllers/UserController..php
@@ -1,14 +1,40 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 include_once "../../config/connectiondb.php";
+include_once "../modules/User.php";
 
+class Database {
+    private $host = "localhost";
+    private $db_name = "taskflow";
+    private $username = "root";
+    private $password = "baoud";
+    private $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch(PDOException $e) {
+            echo "Connection Error: " . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
 
 class UserController {
     private $db;
     private $user;
 
     public function __construct() {
-        $database = new Database();
-        $this->db = $database->getConnection();
+        $conn = new Database();
+        $this->db = $conn->getConnection();
         $this->user = new User($this->db);
     }
 
@@ -17,9 +43,9 @@ class UserController {
             $this->user->username = $_POST['username'];
             $this->user->email = $_POST['email'];
             $this->user->password = $_POST['password'];
-
+            
             if($this->user->create()) {
-                header("Location: src/views/login_form.php");
+                header("Location: ../../src/views/login_form.php");
             } else {
                 return "Registration failed!";
             }
@@ -44,4 +70,7 @@ class UserController {
     }
 }
 
+$res = new UserController() ;
+$res->register();
+$res->login();
 ?>
